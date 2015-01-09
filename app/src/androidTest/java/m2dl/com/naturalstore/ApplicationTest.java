c504d6a820bf6b5a082262312cd440d696f6ffbe
kmarch <kevinmarchois@gmail.com>
2015-01-09 15:49:46 +0100

    Initialisation

---
+++ app/src/androidTest/java/m2dl/com/naturalstore/ApplicationTest.java
@@ -0,0 +1,13 @@
+package m2dl.com.naturalstore;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
