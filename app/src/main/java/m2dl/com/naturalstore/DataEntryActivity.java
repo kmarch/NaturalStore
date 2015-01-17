b4a933235ed559a76d723d8eda97f905a3e64a54
Guinoutortue <guinou@msn.com>
2015-01-17 23:52:28 +0100

    Little correction

--- app/src/main/java/m2dl/com/naturalstore/DataEntryActivity.java
+++ app/src/main/java/m2dl/com/naturalstore/DataEntryActivity.java
@@ -83,9 +83,6 @@ public class DataEntryActivity extends ActionBarActivity implements View.OnClick
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
--- app/src/main/java/m2dl/com/naturalstore/HomeActivity.java
+++ app/src/main/java/m2dl/com/naturalstore/HomeActivity.java
@@ -54,9 +54,6 @@ public class HomeActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
